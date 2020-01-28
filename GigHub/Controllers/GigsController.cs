032ad85183fa75400c04683729fdbd2f1f9685d1
mohsen <kelarens@gmail.com>
2020-01-28 18:11:48 +0330

    saving gigs

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,11 @@
 ﻿using GigHub.Data;
+using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
 
 namespace GigHub.Controllers
@@ -9,11 +13,15 @@ namespace GigHub.Controllers
     public class GigsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public GigsController(ApplicationDbContext context)
+        public GigsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
+        [Authorize]
         public async Task<IActionResult> Create()
         {
             GigsViewModel model = new GigsViewModel
@@ -21,6 +29,38 @@ namespace GigHub.Controllers
                 Genres = await _context.Genres.ToListAsync()
             };
             return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Create(GigsViewModel newGig)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var genre = await _context.Genres.SingleOrDefaultAsync(u => u.Id == newGig.GenreId);
+
+            var gig = new Gig
+            {
+                Artist = user,
+                Genre = genre,
+                Date = DateTime.Parse(string.Format("{0} {1}", newGig.Date, newGig.Time)),
+                Venue = newGig.Venue
+            };
+
+
+            _context.Gigs.Add(gig);
+
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                newGig.Id = gig.Id;
+                return RedirectToAction("index", "Home");
+            }
+
+            return BadRequest();
+        }
+        public IActionResult Save(string @class)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GigHub.Models;
 using Microsoft.AspNetCore.Mvc;
-using GigHub.Models;
+using System.Diagnostics;
 
 namespace GigHub.Controllers
 {
